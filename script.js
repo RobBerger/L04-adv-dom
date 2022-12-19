3e5e4f82c9913b5990a779a12a51c9f7edeffc37
berger <robertberger.dev@gmail.com>
2022-12-19 09:08:29 -0500

    added more examples

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function addClassOne() {
     const divElem1 = document.getElementById('id1');
     divElem1.className = 'classOne';
 }
+
+function addHiddenClass() {
+    const divElem1 = document.getElementById('id1');
+    divElem1.classList.add('hidden');
+}
+
+function removeHidden() {
+    const divElem1 = document.getElementById('id1');
+    divElem1.classList.remove('hidden');
+}
