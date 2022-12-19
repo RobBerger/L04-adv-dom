09dbb49f8484531ca21cfef0432e1ba3f9445573
berger <robertberger.dev@gmail.com>
2022-12-19 09:15:13 -0500

    compleated code

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function removeHidden() {
     const divElem1 = document.getElementById('id1');
     divElem1.classList.remove('hidden');
 }
+
+function toggleClass() {
+    const divElem1 = document.getElementById('id1');
+    // Toggle 'hidden' class
+    divElem1.classList.toggle('hidden')
+}
