bcafff1d2a15dc479726ccf16d67d5ffb22ba53f
berger <robertberger.dev@gmail.com>
2022-12-19 12:22:11 -0500

    new example code

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-function addClassOne() {
-    const divElem1 = document.getElementById('id1');
-    divElem1.className = 'classOne';
-}
-
-function addHiddenClass() {
-    const divElem1 = document.getElementById('id1');
-    divElem1.classList.add('hidden');
-}
-
-function removeHidden() {
-    const divElem1 = document.getElementById('id1');
-    divElem1.classList.remove('hidden');
-}
-
-function toggleClass() {
-    const divElem1 = document.getElementById('id1');
-    // Toggle 'hidden' class
-    divElem1.classList.toggle('hidden')
+function changeColor(){
+    let elem = document.getElementById('cssid');
+    elem.style.color = 'blue';
 }
