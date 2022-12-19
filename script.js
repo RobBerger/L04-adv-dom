670938f3d16f39e6090f398c4e19780b95fb06e1
berger <robertberger.dev@gmail.com>
2022-12-19 16:14:43 -0500

    addEventListener and removeEventListener example

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let myBtn = document.getElementById('myBtn');
 
 myBtn.onclick = function () {
-    alert("FIRST ONCLICK");
-    myBtn.style.color = "blue";
-}
+    alert("FIRST EVENT - HANDLED BY ONCLICK -  CHANGES TEXT COLOR AND BACKGROUND COLOR OF BUTTON");
+    myBtn.style.color = "white";
+    myBtn.style.backgroundColor = "blue"
+} 
 
-myBtn.onclick = function () {
-    alert("SECOND ONCLICK - DYNAMICALLY ADDS A <P> TAG WITH A MESSAGE");
+function changeBtnText() {
+    alert("SECON EVENT - HANDLED BY ADDEVENTLISTENER 1 - CHANGES BUTTON TEXT");
+    myBtn.innerHTML = "I am now a blue button!"
+}
+myBtn.addEventListener("click", changeBtnText);
+
+function sendMessage() {
+    alert("THIRD EVENT - HANDLED BY ADDEVENTLISTENER 2 - DYNAMICALLY ADDS A <P> TAG WITH A MESSAGE");
     let pTag = document.createElement("p");
     pTag.innerHTML = "This is my message.";
     document.body.appendChild(pTag);
 }
+
+myBtn.addEventListener('click', sendMessage);
