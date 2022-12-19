86264d076890cb68f904330088864bccf8612da2
berger <robertberger.dev@gmail.com>
2022-12-19 12:28:43 -0500

    style.width = value

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function changeColor() {
     let elem = document.getElementById('shapeid');
     elem.style.backgroundColor = 'blue';
 }
+
+function changeWidth() {
+    let elem = document.getElementById('shapeid');
+    elem.style.width = '100px';
+}
