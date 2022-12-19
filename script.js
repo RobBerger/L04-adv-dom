3f9faa70cdc643daebd2e7cd6dabf16a63fa4103
berger <robertberger.dev@gmail.com>
2022-12-19 15:44:17 -0500

    added id example

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function clickMe() {
     alert("Hello World!");
   }
+
+  let myBtn = document.getElementById('myBtn');
+myBtn.onclick = function () {
+    alert("Hello World, I am using DOM properties to handle events!");
+} 
