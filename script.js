fa1d70dca4a2cd75917118c03656101bf4185cf1
berger <robertberger.dev@gmail.com>
2022-12-19 15:50:10 -0500

    added a this example

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function clickMe() {
 myBtn.onclick = function () {
     alert("Hello World, I am using DOM properties to handle events!");
 } 
+
+function sayHelloWorld() {
+    alert("Hello World, from a named function!");
+}
+
+let btn = document.getElementById('btn');
+btn.onclick = sayHelloWorld;
